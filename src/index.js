606ab2a9777e321cd2276bfbc30b5a8f4eac5523
ozavcodez <akandeozavize@gmail.com>
2023-12-07 16:42:53 +0100

    made changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 
 var data = []
+let id = 1
 function addNewRowsToTable() {
     
     var tbody = "";
   
     for (var i in data){
-        let id = data[i]._id    
-        // const mem = data[i].member==true?"Yes":"No"
-        const view_record = "viewUser('"+id+"')"
+        let dataId = id++   
+        const view_record = "viewUser('"+dataId+"')"
         // table += "<tr>";
         tbody += "<tr class='divide-x-2 divide-y-2 divide-solid divide-slate-400/25'>"
+        +`<td class='text-center border py-4'>${dataId}</td>`
         +"<td class='text-center border py-4'>" + data[i].fullName + "</td>"
         +"<td class='text-center border py-4'>" + data[i].email + "</td>"
         +"<td class='text-center border py-4'>" + data[i].gender + "</td>"
         +`<td class='text-center  bg-slate-500 py-4' style='justify-items: center;'>
          <button id='edit' class='text-white' onclick=${view_record} style='border-:; '><a href="">Edit</></button>
-         <button id='delete' class='text-white' onclick=${delete_record} style='border-:; '><a href="">Delete</></button>
+         <button id='delete' class='text-white' onclick=${view_record} style='border-:; '><a href="">Delete</></button>
         </td>`
         tbody += "</tr>";
     document.getElementById('result').innerHTML = tbody;
